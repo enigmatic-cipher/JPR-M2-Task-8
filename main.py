aefaa25193b812176bab3d3e5e54c03f37ba2dce
Amit <87896992+enigmatic-cipher@users.noreply.github.com>
2022-01-20 16:46:20 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+"""
+The input string consists of two words separated by a space. Print the two words.
+Input: "Java Master"
+Output:
+Java
+Master
+"""
+
+st = "Java Master"
+for word in st.split():
+  print(word)
